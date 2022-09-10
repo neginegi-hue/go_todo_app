696f6f96c85e85b5358ebed6614962f3cdc0f1ca
105393179+neginegi-hue@users.noreply.github.com <105393179+neginegi-hue@users.noreply.github.com>
2022-09-10 22:40:01 +0900

    run関数を疎結合構成にした

--- main.go
+++ main.go
@@ -4,13 +4,11 @@ import (
 	"context"
 	"fmt"
 	"net"
-	"net/http"
 	"os"
 
 	"log"
 
 	"github.com/neginegi-hue/go_todo_app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -29,24 +27,9 @@ func run(ctx context.Context) error {
 	if err != nil {
 		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
-	url := fmt.Sprintf("http://%s",l.Addr().String())
+	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-			log.Printf("faild to close :%+v", err)
-			return err
-		}
-		return nil
-	})
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("faild to shutdown: %+v", err)
-	}
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.run(ctx)
 }
