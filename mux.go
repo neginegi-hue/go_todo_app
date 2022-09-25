902e71610991b5abe8fe39f15de0c18ebfd5a5a1
105393179+neginegi-hue@users.noreply.github.com <105393179+neginegi-hue@users.noreply.github.com>
2022-09-25 20:48:13 +0900

    エンドポイント完成

--- mux.go
+++ mux.go
@@ -1,13 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+	"github.com/neginegi-hue/go_todo_app/handler"
+	"github.com/neginegi-hue/go_todo_app/store"
+)
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		//静的解析のエラーを回避するため明示的に戻り値を捨てている
+		w.Header().Set("Context-Type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
+	v:= validator.New()
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/task",at.ServeHTTP)
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks",lt.ServeHTTP)
 	return mux
 }
