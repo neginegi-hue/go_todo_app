412002ad34bcebecf6a4390ba17b3e439ab0713b
neginegi-hue <neginegi_hue@gmail.com>
2023-03-05 18:45:47 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	err := http.ListenAndServe(
+		":18000",
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
+		}),
+	)
+	if err != nil {
+		fmt.Printf("faild to terminal server: %v", err)
+		os.Exit(1)
+	}
+}
